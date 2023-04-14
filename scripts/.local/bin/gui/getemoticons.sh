07843e749b22ecd5590e5d40a04f3d251d55d273
Nicholas Defranco <defrann8208@outlook.com>
2023-04-14 17:33:33 -0400

    Make dmenu search case-insensitively

--- scripts/.local/bin/gui/getemoticons.sh
+++ scripts/.local/bin/gui/getemoticons.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-tr '\t' ' ' < ~/.local/bin/gui/emoticons | dmenu | cut -f1 -d' ' | tr -d '\n' | xclip -selection clipboard
+# In case dmenu does not function, read the following comment thread:
+# https://bbs.archlinux.org/viewtopic.php?id=217913
+tr '\t' ' ' < ~/.local/bin/gui/emoticons | dmenu -i | cut -f1 -d' ' | tr -d '\n' | xclip -selection clipboard
